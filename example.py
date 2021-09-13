a4bbd999740c35b6013761e5cebd31d684927eb0
MahdiMF <mmf1374py@gmail.com>
2021-09-14 02:33:49 +0430

    just for beginners

---
+++ example.py
@@ -0,0 +1,10 @@
+# this is an example for a madlib game
+
+last_name = input('Your last name: ')
+adj = input('an adjective: ')
+programming_language = input('The programming language you like: ')
+
+madlib = f"I am Mahdi and my last name is {last_name} . This is so {adj} that I am doing a madlib \
+game and enjoy learning {programming_language}"
+
+print(madlib)
---
+++ main.py
@@ -0,0 +1,9 @@
+# string concatenation (aka how to put strings together)
+
+youtuber = "Mahdi Malekifard"
+
+# a few ways to do this
+
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
